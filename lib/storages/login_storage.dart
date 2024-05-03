efb109d39c0e14040695bcc4f431e60cf7b4b592
wogus1027 <dlwogus1027@naver.com>
2024-05-03 14:38:15 +0900

    로그인 스토리지 추가

---
+++ lib/storages/login_storage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class LoginStorage {
+  static const FlutterSecureStorage storage = FlutterSecureStorage();
+
+  static const _keyLoginDataId = "loginId";
+  static const _keyLoginDataPw = "loginPw";
+  static const _keyLoginDataType = "loginType";
+
+  static Future saveLoginData(
+      {required String id, required String pw, required bool isStored}) async {
+    await storage.write(key: _keyLoginDataId, value: id);
+    await storage.write(key: _keyLoginDataPw, value: pw);
+    await storage.write(
+        key: _keyLoginDataType, value: isStored ? "true" : "false");
+  }
+
+  static Future<List<String?>> getLoginData() async {
+    final id = await storage.read(key: _keyLoginDataId);
+    final pw = await storage.read(key: _keyLoginDataPw);
+    final isStored = await storage.read(key: _keyLoginDataType);
+    return [
+      id,
+      pw,
+      isStored,
+    ];
+  }
+
+  // 이메일을 비움
+  static Future deleteLoginData() async {
+    await storage.delete(key: _keyLoginDataId);
+    await storage.delete(key: _keyLoginDataPw);
+    await storage.delete(key: _keyLoginDataType);
+  }
+}
